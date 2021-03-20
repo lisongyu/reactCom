2cf69b5d3c1482cadb435790611e0079f777df03
lisongyu <lisongyu@allinmd.cn>
2021-03-20 10:50:01 +0800

    提及饿哦

--- src/components/CreatePortal/index.js
+++ src/components/CreatePortal/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 export default class CreatePortal extends Component {
-
   constructor(props) {
     super(props);
     this.body = document.querySelector('body')
